01b681d6e79c5f2040c48f8d16973199d2f0c7cd
Letladi Sebesho <letladi_sebesho@discovery.com>
2021-11-15 15:10:44 +0200

    replaced hardcoded repo items

--- src/features/repos/Repos.js
+++ src/features/repos/Repos.js
@@ -6,7 +6,7 @@ import { Container, Row, Col } from "react-grid-system";
 import Button from "../../components/Button/Button";
 import './Repos.scss';
 
-function RepoItem() {
+function RepoItem(props) {
   return (
     <div className="repo-item">
       <Container>
@@ -16,37 +16,35 @@ function RepoItem() {
               <div className="repo-title">
                 <HiOutlineBookmarkAlt />
                 <a href="#" target="_blank">
-                  huggingface / <span>datasets</span>
+                  {props.username} / <span>{props.repositoryName}</span>
                 </a>
               </div>
               <Button icon={<HiOutlineStar />} title="Star" />
             </header>
             <div className="description">
-              🤗 The largest hub of ready-to-use datasets for ML models with
-              fast, easy-to-use and efficient data manipulation tools
+              {props.description}
             </div>
             <footer>
               <div className="meta">
-                <div className="meta-value">Python</div>
+                <div className="meta-value">{props.language}</div>
                 <div className="meta-value">
-                  <HiOutlineStar /> 11,150
+                  <HiOutlineStar /> {props.totalStars}
                 </div>
                 <div className="meta-value">
-                  <BiGitRepoForked /> 1,304
+                  <BiGitRepoForked /> {props.forks}
                 </div>
                 <div className="meta-value">
                   Built by
                   <span className="developer-avatars">
-                    {Array(4)
-                      .fill("https://picsum.photos/50/50")
-                      .map((url) => (
-                        <img src={url} alt="dev thumbnail" />
+                    {props.builtBy
+                      .map(({ avatar, username }) => (
+                        <img src={avatar} alt={`thumbnail for ${username}`} />
                       ))}
                   </span>
                 </div>
               </div>
               <div className="stars-today">
-                <HiOutlineStar /> 212 stars today
+                <HiOutlineStar /> {props.starsSince} stars today
               </div>
             </footer>
           </Col>
@@ -67,7 +65,10 @@ function Repos() {
 
   return (
     <>
-      <RepoItem />
+      {data.map(repo => (
+        <RepoItem {...repo} />
+      ))}
+
     </>
   );
 }
